9394e489083655e6963ec2650e6de93f0e6290b2
sushil <jambilateralus@gmail.com>
2016-01-18 12:40:55 +0545

    toggle button beta

--- app/src/main/java/com/project/myschedule/TaskList.java
+++ app/src/main/java/com/project/myschedule/TaskList.java
@@ -50,6 +50,6 @@ public class TaskList extends AppCompatActivity {
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.tasklist, menu);
-        return true;
+        return true;//
     }
 }
