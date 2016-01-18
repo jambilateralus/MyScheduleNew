c7357db16c146fbae443887fb21c27d74ee62a05
sushil <jambilateralus@gmail.com>
2016-01-18 13:04:53 +0545

    toggle button beta

--- app/src/main/java/com/project/myschedule/AddTask.java
+++ app/src/main/java/com/project/myschedule/AddTask.java
@@ -1,7 +1,10 @@
 package com.project.myschedule;
 
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class AddTask extends AppCompatActivity {
 
@@ -13,6 +16,34 @@ public class AddTask extends AppCompatActivity {
         //Set title
         setTitle("New Task");
 
+        //Back arrow in action bar
+        final ActionBar actionBar = getSupportActionBar();
+        if (null != actionBar) {
+            actionBar.setDisplayHomeAsUpEnabled(true);
+        }
+
 
     }
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        //on back button clicked
+        if (id == android.R.id.home) {
+            //startActivity(new Intent(getBaseContext(),MainActivity.class));
+            finish();
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.addtask, menu);
+        return true;
+    }
+
 }
